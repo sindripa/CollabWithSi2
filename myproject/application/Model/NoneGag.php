c6efde1a683e30b7fdfcf3eb86fe988db4c94cce
Sindri Pálsson <sindripa234@nemi.tskoli.is>
2017-04-25 10:10:43 +0000

    Nonegag plank page

--- myproject/application/Model/NoneGag.php
+++ myproject/application/Model/NoneGag.php
@@ -5,57 +5,14 @@ namespace Mini\Model;
 
 use Mini\Core\Model;
 
-class UserEdit extends Model
+class NoneGag extends Model
 {
-    public function addUser($nafn, $pass, $email)
+    public function InitNoneGag()
     {
-        $hashedPass = password_hash($pass, PASSWORD_DEFAULT);
-        $sql = "INSERT INTO notandi(nafn, adgangsord, netfang)  VALUES (:nafn, :pass, :email )";
-        $query = $this->db->prepare($sql);
-        $parameters = array(':pass' => $hashedPass, ':nafn' => $nafn, ':email' => $email);
-        $query->execute($parameters);
+        
     }
-    public function authenticate($nafn, $passi)
+    public function LoadMore()
     {
-        if (isset($nafn)&&isset($passi)) {
-            $sql = "select U_id, U_name, U_password FROM Usor where U_name = :name LIMIT 1";
-            $query = $this->db->prepare($sql);
-            $parameters = array(':name' => $nafn);
-            $query->execute($parameters);
-            $user = $query->fetch();
-            if (isset($user->U_name)) {//does exsist?
-                if (password_verify($passi,$user->U_password)) {
-                    if(session_status() == PHP_SESSION_NONE) session_start();
-                    $_SESSION['authenticatedID'] = $user->U_id;
-                    $_SESSION['username'] = $user->U_name;
-                    //$_SESSION['start'] = time(); er ekki að fíla timelimit
-                    $_SESSION['adminBool'] = $user->admin;
-                }
-            }
-        }
-        else{
-            return false;
-        }
-    }
-    
-    public function getAllUsers()
-    {
-        $sql = "select nafn, netfang from notandi";
-        $query = $this->db->prepare($sql);
-        $query->execute();
-        return $query->fetchAll();
-    }
-    public function EditName()
-    {
-        if (isset($_POST["newName"])) {
-            $sql = "UPDATE notandi SET nafn = :nafn WHERE id = :user_ID";
-            $query = $this->db->prepare($sql);
-            $parameters = array(':nafn' => $_POST["newName"], ':user_ID' =>  $_POST["update"]);
-            $query->execute($parameters);
-        }
-        else{
-            echo "new name not set";
-            echo $_SESSION['authenticatedID'];
-        }
+        
     }
 }
--- myproject/application/view/_templates/footer.php
+++ myproject/application/view/_templates/footer.php
@@ -1,20 +1,3 @@
-
-    <div class="footer">
-        Find <a href="https://github.com/panique/mini3">MINI3 on GitHub</a>.
-        If you like the project, support it by <a href="http://tracking.rackspace.com/SH1ES">using Rackspace</a> as your hoster [affiliate link].
-    </div>
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-    <script>
-        var url = "<?php echo URL; ?>";
-    </script>
-
-    <!-- our JavaScript -->
-    
-    <?php
-    if(session_status() == PHP_SESSION_NONE) session_start();
-     if (isset($_SESSION['authenticatedID'])) { 
-        echo '<script>var nafn = "'. $_SESSION['username'] .'";</script>';
-        echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script><script src="<?php echo URL; ?>js/loading.js"></script>';
-    } ?>
-</body>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    </body>
 </html>
