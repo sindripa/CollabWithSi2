715df8c197c04c31b480a3ff909e961478d80f97
Sigurður Orri Hjaltason <sigurdurhj320@nemi.tskoli.is>
2017-05-03 15:12:51 +0000

    Add files via upload

--- SQL/databaseCodi.sql
+++ SQL/databaseCodi.sql
@@ -10,10 +10,10 @@ create table Post
 (
     P_id int primary key AUTO_INCREMENT,
     P_title varchar(128),
-    P_url varchar(15),
+    P_url varchar(15) not null,
     P_upp int default 0,
-    P_date DATETIME,
-    U_id int,
+    P_date DATETIME default CURRENT_TIMESTAMP,
+    U_id int not null,
     FOREIGN KEY (U_id) REFERENCES Usor(U_id)
 );
 create table Votes
@@ -40,3 +40,29 @@ create table Reply
     R_txt varchar(500),
     FOREIGN KEY (C_id) REFERENCES Commint(C_id)
 );
+
+select P_title, P_url, P_upp, P_id from Post where P_id < :range ORDER BY P_id DESC limit 8 /*:range is the id of the last loaded post*/
+
+
+delimiter ☺
+create trigger VotingHandler
+    after insert on Votes
+        update Post
+        set P_upp = (select P_upp from Post where P_id = new.P_id)+new.V_value;
+        where P_id = new.P_id;
+delimiter ;
+
+delimiter ☺
+create trigger VotingHandler
+    before delete on Votes
+    update Post
+        set P_upp = (select P_upp from Post where P_id = old.P_id)-old.V_value;
+        where P_id = old.P_id;
+delimiter ;
+
+delimiter ☺
+create trigger VotingHandler
+    before insert on Votes
+
+
+delimiter ;
