1eb6f504019d22b15abfd5afa2eab3e49278e5e4
sindripa <sindripa234@nemi.tskoli.is>
2017-05-07 14:53:33 +0000

    Revert "does this work now?"
    This reverts commit c3273645718bd3231205d3af518a3c1ca26fb27a, reversing
    changes made to f4071f446cd2eb7f735cf938d712aeed24052132.

--- myproject/application/Controller/NonegagController.php
+++ myproject/application/Controller/NonegagController.php
@@ -11,6 +11,7 @@ class NonegagController
 		$NonegagModel = new NoneGag();
 		if(session_status() == PHP_SESSION_NONE) session_start();
 		$newest = $NonegagModel->TopId()->fuck;
+        print_r($_SESSION);
 		require APP . 'view/_templates/header.php';
 		require APP . 'view/Nonegag/index.php';
 		require APP . 'view/_templates/footer.php';
@@ -34,8 +35,4 @@ class NonegagController
 		$NonegagModel->Voting($inputs[0],$inputs[1]);
 	}
 	public function test(){$NonegagModel = new Nonegag();$newest = $NonegagModel->TopId()->fuck; echo $newest;}
-	public function privatePost($postID)
-	{
-		
-	}
 }
